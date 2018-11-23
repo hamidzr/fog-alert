243d46e8cd5cc2c463b65928cdbe539084826e84
Hamid Zare <dellydela@gmail.com>
2018-11-22 19:34:27 -0600

    WIP posting image capability

--- edge/test.py
+++ edge/test.py
@@ -1,5 +1,7 @@
 from picamera import PiCamera
 from time import sleep
+from PIL import ImageGrab
+from StringIO import StringIO
 
 camera = PiCamera()
 
@@ -7,3 +9,20 @@ for i in range(5):
   sleep(5)
   camera.capture('/home/pi/image%s.jpg' % i)
   camera.stop_preview()
+
+
+
+def send_image():
+  content_type = 'image/jpeg'
+  headers = {'content-type': content_type}
+
+  screen = ImageGrab.grab()
+  img = stringIO()
+  screen.save(img, 'JPEG')
+  img.seek(0)
+  fd = open('screen.jpg', 'wb')
+  fd.write(img.getvalue())
+  fd.close()
+  fin = open('screen.jpg', 'wb')
+  files = {'file': ('test.jpg', img, content_type)}
+  requests.post('http://localhost/upload', files=files)
