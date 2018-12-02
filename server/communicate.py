6454028360d44d7a0671bbb4c9d80fcc7821e8cf
Hamid Zare <dellydela@gmail.com>
2018-12-01 20:19:09 -0600

    added mqtt client connection to publish detected threats

--- server/communicate.py
+++ server/communicate.py
@@ -3,12 +3,28 @@
 """
 publishes detection of a threat to subscribers
 """
-import time
 
+import paho.mqtt.client as paho
+
+# TODO refactor to use the config file?
+BROKER = "broker.hivemq.com"
+TOPIC = "hph/threat"
+
+mqtt_client= paho.Client("detector-client")
+
+def on_message(client, userdata, message):
+    print("received message =",str(message.payload.decode("utf-8")))
+"localhost" #
 def trigger_response():
-  print('sending threat response request..')
-  time.sleep(0.5)
+  print("publishing the threat")
+  mqtt_client.publish(TOPIC,"detected")
 
+# connect to the broker
+print("connecting to broker ",BROKER)
+mqtt_client.connect(BROKER)
+
+# remember to close the connection on shutdown
 
 if __name__ == '__main__':
   trigger_response()
+  mqtt_client.disconnect()
