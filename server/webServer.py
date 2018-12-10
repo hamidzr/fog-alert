daa6bd64adbfa5d0353f018a687cdb1a9cf0cd5e
Hamid Zare <dellydela@gmail.com>
2018-12-09 18:30:45 -0600

    changed how detectors are imported, easier swappin

--- server/webServer.py
+++ server/webServer.py
@@ -1,5 +1,5 @@
 from flask import request, Flask
-import detectors.colorMaskDetector as colorMaskDetector
+from detectors.colorMaskDetector import has_intruder
 from communicate import trigger_response
 import configparser
 import os
@@ -33,7 +33,7 @@ def upload():
   file_name = sanitize_name(posted_file)
   save_path = UPLOAD_DIR + '/' + file_name
   posted_file.save(save_path)
-  detected = colorMaskDetector.has_intruder(save_path)
+  detected = has_intruder(save_path)
   # TODO trigger w/o blocking or trigger response elsewhere
   print('threat:', detected)
   if detected: trigger_response()
